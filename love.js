94c0aa9aa2c5a11214ab905ebfb04e823869889d
Prince Agrahari <princeagrahari2000@gmail.com>
2021-08-16 11:02:57 +0000

    s

--- love.js
+++ love.js
@@ -1,9 +1,11 @@
 const request = require('request');
-
+let fname=document.getElementById('fname');
+let lname=document.getElementById('lname')
+let result=document.getElementById('result')
 const options = {
   method: 'GET',
   url: 'https://love-calculator.p.rapidapi.com/getPercentage',
-  qs: {fname: 'John', sname: 'Alice'},
+  qs: {fname: fname, sname: lname},
   headers: {
     'x-rapidapi-key': '4862aa2e37msh30b060dbaffcaf2p13d7cbjsn9096b49893df',
     'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
